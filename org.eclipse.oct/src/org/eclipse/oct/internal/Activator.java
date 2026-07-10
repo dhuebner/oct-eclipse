c77d57b352c359e70eed58350af7d1b0c62443cd
Dennis Huebner <dennis.huebner@gmail.com>
2026-07-10 10:26:52 +0200

    Plug-in project initial

---
+++ org.eclipse.oct/src/org/eclipse/oct/internal/Activator.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 TypeFox GmbH and others.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ *
+ */
+package org.eclipse.oct.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * 
+ * Main activation entry point
+ * 
+ * @author Dennis Hübner - Initial contribution and API.
+ */
+public class Activator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext arg0) throws Exception {
+
+	}
+
+	@Override
+	public void stop(BundleContext arg0) throws Exception {
+
+	}
+
+}
